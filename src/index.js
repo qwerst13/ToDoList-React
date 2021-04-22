df69a08f9a60941392d190f6208745d71e39c5ff
Heizenberg <qwerst13@gmail.com>
2021-04-22 22:14:14 +0300

    'Initialization'

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const TodoList = () => {
+    return (
+        <ul>
+            <li>First</li>
+            <li>Second</li>
+        </ul>
+    )
+};
+
+
+
