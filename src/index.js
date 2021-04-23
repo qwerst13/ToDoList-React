e0d3eb85fe359631872a0d0c2cc82197b53b214a
Heizenberg <qwerst13@gmail.com>
2021-04-23 23:49:37 +0300

    clock added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const TodoList = () => {
+import Title from './components/Title';
+import Search from './components/Search';
+import TodoList from './components/TodoList';
+import Clock from './components/Clock'
+
+const App = () => {
+
+    const data =  [
+        {label: 'First step', id: '1'},
+        {label: 'Second something', id: '2'},
+        {label: 'Third wohoo!', id: '3'}
+    ];
+
     return (
-        <ul>
-            <li>First</li>
-            <li>Second</li>
-        </ul>
-    )
+        <div>
+            <Title />
+            <Clock />
+            <Search />
+            <TodoList data = { data } />
+
+        </div>
+    );
 };
 
-
+ReactDOM.render(<App />, document.getElementById('root'));
 
