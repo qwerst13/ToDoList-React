2047141c8f0aaa227a95aadc94b3aae3973f4828
Heizenberg <qwerst13@gmail.com>
2021-04-26 21:53:11 +0300

    adding state for Task completion

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -4,22 +4,37 @@ import './Task.css';
 
 
 export default class Task extends React.Component {
+
+    state = {completed: false};
+
+    completeTask = () => {
+        this.setState({completed: !this.state.completed});
+        console.log(this.state)
+    };
+
     render () {
-        const {description, date} = this.props;
+        const {className, description, date} = this.props;
+        const {completed} = this.state;
+
+        let liClassName = '';
+
+        if (completed) liClassName = 'completed';
 
         return (
-            <div>
-                <div className="view">
-                    <input className="toggle" type="checkbox"/>
-                    <label>
-                        <span className="description">{ description }</span>
-                        <span className="created">{ date.toLocaleTimeString() }</span>
-                    </label>
-                    <button className="icon icon-edit"/>
-                    <button className="icon icon-destroy"/>
+            <li className={liClassName}>
+                <div>
+                    <div className="view">
+                        <input onClick={this.completeTask} className="toggle" type="checkbox"/>
+                        <label>
+                            <span className="description">{ description }</span>
+                            <span className="created">{ date.toLocaleTimeString() }</span>
+                        </label>
+                        <button className="icon icon-edit"/>
+                        <button className="icon icon-destroy"/>
+                    </div>
+                    <input type="text" className="edit" value="Editing task"/>
                 </div>
-                <input type="text" className="edit" value="Editing task"/>
-            </div>
+            </li>
         );
     }
 }
